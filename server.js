cfcd1b2777f4d938e4f0c6953e739bc8c20c480e
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-03-12 20:10:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,65 +1,358 @@
-var express = require('express')
-var cors = require('cors')
-const path = require('path')
-require('dotenv').config()
-const multer = require('multer')
-// const upload = multer({ dest: './public/data/uploads' })
+// const { connectToDb, getDb } = require('./db')
+// const { ObjectId } = require('mongodb')
 
-var app = express()
 
-const { connectToDb, getDb } = require('./db')
-const { ObjectId } = require('mongodb')
+// var express = require('express')
+// var cors = require('cors')
+// const path = require('path')
+// require('dotenv').config()
+// const multer = require('multer')
+
+// var app = express()
+
+
+// // db connection
+// let db
+// connectToDb((err) => {
+//     if (!err) {
+//         app.listen(process.env.PORT || 3000, () => {
+//         })
+//         db = getDb()
+//     }
+// })
+
+// // const port = process.env.PORT || 3000
+// // app.listen(port, function () {
+// //     console.log('Your app is listening on port ' + port)
+// // })
+
+// app.use(express.json())
+// app.use(cors())
+// app.use(express.static(__dirname + '/public'))
+// app.use(express.urlencoded({ extended: true }))
+// app.set('view engine', 'ejs')
+
+// app.get('/', (req, res) => {
+
+//     let blogs = []
+//     db.collection('blogs')
+//         .find()
+//         .sort({ _id: -1 })
+//         .forEach(blog => blogs.push(blog))
+//         .then(() => {
+
+//             res.render(path.join(__dirname + '/views/index.ejs'), { articles: blogs })
+//             // res.render('index', { articles: blogs })
+//         })
+//         .catch((err) => {
+//             res.status(500).json({ error: 'could not get data' })
+//         })
+
+//     // res.render(path.join(__dirname + '/views/index.ejs'))
+// })
+
+// // for deleting down
+// app.post('/', (req, res) => {
+
+
+//     db.collection('blogs')
+//         .deleteOne({ _id: ObjectId(req.query.id) })
+//         .then(result => {
+
+//             res.status(200)
+
+//             res.redirect('/');
+//         })
+//         .catch(err => {
+//             res.status(500).json({ error: 'Could not delete task' })
+//         })
+
+
+// })
+
+// // app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+// //   const fileInfo = req.file
+
+// //   res.json({
+// //     name: fileInfo.originalname,
+// //     type: fileInfo.mimetype,
+// //     size: fileInfo.size,
+// //   })
+// // })
+
+// app.get('/new', (req, res) => {
+//     res.render('new')
+//     // res.send("okayyyyyt")
+
+// })
+
+
+// app.get('/features', (req, res) => {
+//     let updates = []
+//     db.collection('updates')
+//         .find()
+//         .sort({ _id: -1 })
+//         .forEach(blog => updates.push(blog))
+//         .then(() => {
+
+//             res.render('features', { updates: updates })
+//         })
+//         .catch((err) => {
+//             res.status(500).json({ error: 'could not get data' })
+//         })
+
+// })
+
+// app.get('/edit', (req, res) => {
+
+//     let bh = req.query.id
+
+
+//     let blogs = []
+//     db.collection('blogs')
+//         .findOne({ _id: ObjectId(req.query.id) })
+//         .then(result => {
+//             res.render('edit', { articles: result })
+//         })
+//         .catch((err) => {
+//             res.status(500).json({ error: 'could not get data' })
+//         })
+
+//     let blog = { title: "Temporary title", description: "Temporary description", markdown: 'Temporary markdown' }
+
+
+// })
+
+// app.post('/edit', (req, res) => {
+
+//     let updatedTitle = req.body.title
+//     let updatedDescription = req.body.description
+//     let updatedMarkdown = req.body.markdown
+
+//     db.collection('blogs')
+
+//         .findOneAndUpdate({ _id: ObjectId(req.body.id) }, {
+
+//             $set: {
+//                 "title": updatedTitle,
+//                 "description": updatedDescription,
+//                 "markdown": updatedMarkdown
+//             }
+//         })
+//         .then(result => {
+
+//             res.redirect(`/article?v=${req.body.id}`)
+//         })
+//         .catch((err) => {
+
+//             res.status(500).json({ error: 'could not get data' })
+//         })
+// })
+
+// app.post('/article', (req, res) => {
+
+//     let newBlog = req.body
+
+
+
+//     db.collection('blogs')
+//         .insertOne(newBlog)
+//         .then(result => {
+
+
+//             var stringID = req.body._id.toString()
+
+
+//             res.redirect(`/article?v=${stringID}`)
+
+
+
+
+//             res.status(200)
+
+//         })
+//         .catch(err => {
+//             res.status(500)
+//             res.send(`Error:${err}`)
+//         })
+
+// })
+
+// app.get('/article', (req, res) => {
+
+//     db.collection('blogs')
+//         .findOne({ _id: new ObjectId(req.query.v) })
+//         .then(result => {
+
+//             if (result) {
+//                 res.render('article', { articles: result })
+//             } else {
+
+//                 res.redirect('/')
+//             }
+
+//         })
+
+
+
+
+
+// })
+
+
+// module.exports = app
+
+const { connectToDb, getDb } = require('./db');
+const { ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+const multer = require('multer');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const dbCollection = {
+    blogs: 'blogs',
+    updates: 'updates',
+};
 
 // db connection
-let db
-connectToDb((err) => {
+let db;
+connectToDb(async (err) => {
     if (!err) {
-        app.listen(process.env.PORT || 3000, () => {
-        })
-        db = getDb()
+        await app.listen(PORT);
+        db = getDb();
     }
-})
+});
 
-// const port = process.env.PORT || 3000
-// app.listen(port, function () {
-//     console.log('Your app is listening on port ' + port)
-// })
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static(__dirname + '/public'))
-app.use(express.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
+app.get('/', async (req, res) => {
+    try {
+        const blogs = await db
+            .collection(dbCollection.blogs)
+            .find()
+            .sort({ _id: -1 })
+            .toArray();
 
-app.get('/', (req, res) => {
+        res.render(path.join(__dirname + '/views/index.ejs'), { articles: blogs });
+    } catch (err) {
+        res.status(500).json({ error: 'could not get data' });
+    }
+});
 
-    let blogs = []
+app.post('/', async (req, res) => {
+    try {
+        await db
+            .collection(dbCollection.blogs)
+            .deleteOne({ _id: ObjectId(req.query.id) });
+        res.redirect('/');
+    } catch (err) {
+        res.status(500).json({ error: 'Could not delete task' });
+    }
+});
+
+app.get('/new', (req, res) => {
+    res.render('new');
+});
+
+app.get('/features', async (req, res) => {
+    try {
+        const updates = await db
+            .collection(dbCollection.updates)
+            .find()
+            .sort({ _id: -1 })
+            .toArray();
+        res.render('features', { updates });
+    } catch (err) {
+        res.status(500).json({ error: 'could not get data' });
+    }
+});
+
+app.get('/edit', async (req, res) => {
+    try {
+        const result = await db
+            .collection(dbCollection.blogs)
+            .findOne({ _id: ObjectId(req.query.id) });
+        res.render('edit', { articles: result });
+    } catch (err) {
+        res.status(500).json({ error: 'could not get data' });
+    }
+});
+
+app.post('/edit', async (req, res) => {
+    try {
+        const updatedTitle = req.body.title;
+        const updatedDescription = req.body.description;
+        const updatedMarkdown = req.body.markdown;
+        await db.collection(dbCollection.blogs).findOneAndUpdate(
+            { _id: ObjectId(req.body.id) },
+            {
+                $set: {
+                    title: updatedTitle,
+                    description: updatedDescription,
+                    markdown: updatedMarkdown,
+                },
+            }
+        );
+        res.redirect(`/article?v=${req.body.id}`);
+    } catch (err) {
+        res.status(500).json({ error: 'could not get data' });
+    }
+});
+
+
+app.post('/article', (req, res) => {
+
+    let newBlog = req.body
+
+
+
     db.collection('blogs')
-        .find()
-        .sort({ _id: -1 })
-        .forEach(blog => blogs.push(blog))
-        .then(() => {
+        .insertOne(newBlog)
+        .then(result => {
 
-            res.render(path.join(__dirname + '/views/index.ejs'), { articles: blogs })
-            // res.render('index', { articles: blogs })
+
+            var stringID = req.body._id.toString()
+
+
+            res.redirect(`/article?v=${stringID}`)
+
+
+
+
+            res.status(200)
+
         })
-        .catch((err) => {
-            res.status(500).json({ error: 'could not get data' })
+        .catch(err => {
+            res.status(500)
+            res.send(`Error:${err}`)
         })
 
-    // res.render(path.join(__dirname + '/views/index.ejs'))
 })
 
-// app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-//   const fileInfo = req.file
+app.get('/article', (req, res) => {
 
-//   res.json({
-//     name: fileInfo.originalname,
-//     type: fileInfo.mimetype,
-//     size: fileInfo.size,
-//   })
-// })
+    db.collection('blogs')
+        .findOne({ _id: new ObjectId(req.query.v) })
+        .then(result => {
+
+            if (result) {
+                res.render('article', { articles: result })
+            } else {
+
+                res.redirect('/')
+            }
+
+        })
 
 
 
-module.exports = app
+
+
+})
